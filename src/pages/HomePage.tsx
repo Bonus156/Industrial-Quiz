fdef36ac0533f6dfce4a5b9cbdf6e0a8f9e3eebf
Bonus156 <Bonus.by@mail.ru>
2023-07-19 23:08:46 +0300

    feat: add link to themes at home page, add startExamButton

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 function HomePage() {
   return (
     <div className="container mx-auto flex-grow">
       <h1>Home</h1>
       <p>Home page</p>
+      <Link to={'/themes'}>Перейти к списку доступных тем проверки знаний</Link>
     </div>
   )
 }
