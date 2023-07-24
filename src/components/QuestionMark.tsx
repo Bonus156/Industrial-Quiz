f556f6fa2493a9879f27464dd709d30f4739c4f6
Bonus156 <Bonus.by@mail.ru>
2023-07-24 22:57:03 +0300

    feat: border-2 for active question

--- src/components/QuestionMark.tsx
+++ src/components/QuestionMark.tsx
@@ -16,10 +16,12 @@ export function QuestionMark(props: QMProps) {
 
   return (
     <li className={`nav-item relative cursor-pointer shrink-0 list-decimal list-inside w-[var(--nav-element-w)] h-[var(--nav-element-h)] text-center ${props.num === props.questNum ? 'border-2' : 'border'} border-solid rounded border-black hover:underline`}>
-      <div className={`${qState[props.num].isAnswered ? qState[props.num].isCorrect ? 'correct' : 'incorrect' : ''} correct-mark w-[calc(var(--nav-element-w)-2*var(--border-v))] h-[calc(var(--nav-element-h)/2-2*var(--border-v))] rounded-b-sm absolute bottom-0 after:absolute after:bottom-1 after:left-2.5 after:border-solid after:border-white after:border-r-3 after:border-b-3 after:rotate-45 after:w-2 after:h-3`}></div>
+      <div className={`${qState[props.num].isAnswered ? qState[props.num].isCorrect ? 'correct' : 'incorrect' : ''} correct-mark w-full h-1/2 rounded-b-sm absolute bottom-0 after:absolute after:border-solid after:border-white after:border-r-3 after:border-b-3 after:rotate-45 after:w-2 after:h-3 after:inset-x-1/3 after:bottom-1.5`}></div>
     </li>
   )
 }
+
+// w-[calc(var(--nav-element-w)-2*var(--border-v))] h-[calc(var(--nav-element-h)/2-2*var(--border-v))]
 
 export function AnswerMark(props: AMProps) {
   const qState: QState[] = useContext(QuestionContext);
