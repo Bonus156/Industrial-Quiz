2151b7472d2c5f8986f643ad2e409031563deca2
Bonus156 <Bonus.by@mail.ru>
2023-12-14 15:44:04 +0300

    refactor: del copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'vite-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    copy([
-      { src: 'src/assets/images', dest: 'dist/assets/images' },
-    ]),
-  ],
+  plugins: [react()],
 })
