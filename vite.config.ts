acb09721bcd566bc45c1bffdc2c1a498e64d4502
Bonus156 <Bonus.by@mail.ru>
2023-12-14 15:30:37 +0300

    feat: add copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'vite-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy([
+      { src: 'src/assets/images', dest: 'dist/assets/images' },
+    ]),
+  ],
 })
