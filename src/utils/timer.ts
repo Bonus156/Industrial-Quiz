bef08cfc32622fccd66fcc2c6fecdce8a6b02989
Bonus156 <Bonus.by@mail.ru>
2024-11-09 00:05:28 +0300

    add one year term

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,4 +1,4 @@
-export const endDate = new Date(2024, 9, 30, 17, 0, 0);
+export const endDate = new Date(2025, 9, 30, 17, 0, 0);
 
 export function timer() {
   const restSeconds = Math.floor((endDate.getTime() - new Date().getTime())/1000);
